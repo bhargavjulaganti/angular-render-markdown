8df0b49d3bf6fbcd7aa3173d4a2a7c3b73de8cf0
bhargava julaganti <bhargavjulaganti@gmail.com>
2025-05-11 10:39:52 -0400

    show markdown once clicked on  a link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MarkdownModule],
- template: `
-    <markdown src="assets/example.md"></markdown>
+  imports: [CommonModule, MarkdownModule],
+  template: `
+    <a href="#" (click)="loadMarkdown($event)">Click here to see documentation</a>
+
+    <div *ngIf="showMarkdown">
+      <markdown [src]="markdownPath"></markdown>
+    </div>
   `
 })
-export class AppComponent {}
+export class AppComponent {
+  showMarkdown = false;
+  markdownPath = 'assets/example.md';
+
+  loadMarkdown(event: Event) {
+    event.preventDefault(); // Prevent page reload
+    this.showMarkdown = true;
+  }
+}
